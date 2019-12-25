2bd4a48708b645b50d98d7b3faed92080e31993d
Timo <artur.fiedler54@gmail.com>
2019-12-26 00:11:29 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const urlRegex = new RegExp("[?&]url=(.+)");
+
+addEventListener('fetch', event => {
+    event.respondWith(handleRequest(event.request))
+});
+
+async function handleRequest(request) {
+    console.log(request)
+    try {
+        if (!urlRegex.test(request.url)) {
+            return new Response(JSON.stringify({
+                message: "no url specified. set url query"
+            }), {
+                status: 400,
+                headers: {
+                    "content-type": "application/json"
+                }
+            })
+        } else {
+            const [, url] = request.url.match(urlRegex),
+                headers = {};
+            for (const [k, v] of request.headers.entries()) {
+                headers[k] = v;
+            }
+            const req = await fetch(url, {
+                headers,
+                method: request.method,
+                body: request.bodyUsed ? request.body : undefined
+            });
+
+            return new Response(await req.text(), {
+                status: req.status,
+                headers: {
+                    "content-type": req.headers.get("content-type")
+                }
+            });
+        }
+    } catch (e) {
+        return new Response(e.stack, {
+            status: 500,
+            headers: {
+                "content-type": "application/json"
+            }
+        })
+    }
+}
